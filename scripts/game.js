0c92ad3570937fd9260c4ec8d3daede1f99db56b
jam-ally <jamallarocque@hotmail.com>
2025-08-11 02:50:28 +0100

    initial game window structure, basic player & obstacle shapes and x-y axes

---
+++ scripts/game.js
@@ -0,0 +1,28 @@
+// function startGame () {
+    //begin screen movement 
+    //generate obstacles
+    // monitor for movement 
+    //monitor for interactions with obstacles
+    //count score 
+
+// }
+
+// function playerMovement (player) {
+
+//  player active and visible 
+// player movement - along the y -axis 
+                // - along the x axis 
+//     
+// }
+
+let player = document.getElementsByClassName("stone-piercer");
+
+
+
+// $(".developer-mode").click(function(), {
+//     $(".obstacles").addClassName("hidden");    
+// })
+
+
+
+
