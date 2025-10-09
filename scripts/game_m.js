9d9e4770497abadfb5f4150c2675b28fd27c5d5c
jam-ally <jamallarocque@hotmail.com>
2025-10-09 13:07:09 +0100

    further updates

--- scripts/game_m.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load',function(){
-    const canvas = this.document.getElementById('canvas1');
-    const ctx = getContext('2d');
-    canvas.width =  500;
-    canvas.height = 500;
-
-
-});
